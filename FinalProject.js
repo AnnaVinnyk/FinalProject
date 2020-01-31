a83eab93fcfe592b4dd9b767bc024cb399bba359
AnnaVinnyk <59229096+AnnaVinnyk@users.noreply.github.com>
2020-02-01 00:58:34 +0200

    Add files via upload

--- FinalProject.js
+++ FinalProject.js
@@ -15,15 +15,13 @@
 
 
 let menuElem = document.getElementById('OurService');
+let subMenuElem = document.getElementById('sub');
 let titleElem = menuElem.querySelector('.service');
 
 titleElem.onclick = function () {
     if (window.innerWidth <= 700) {
-        sub.style.position = "relative";
+        subMenuElem.classList.toggle('open');
         menuElem.classList.toggle('open');
-    }
-    else {
-        sub.style.position = "absolute";
-        menuElem.classList.toggle('');
+        flag = 0;
     }
 }
