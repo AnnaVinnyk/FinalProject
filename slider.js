4c8ccf89f3c2a08678d34ba5db90839918444f0e
AnnaVinnyk <59229096+AnnaVinnyk@users.noreply.github.com>
2020-02-13 06:49:04 +0200

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,42 @@
+let slideIndex = 0,
+    buttonIndex = 0;
+
+function changeIndex(newIndex) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides-01");
+    let buttons = document.getElementsByClassName("karusel-navigator");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.opacity = "0.0";
+        buttons[i].className = "karusel-navigator white-on-navy";
+    }
+    if (newIndex) { slideIndex = newIndex; }
+    else { slideIndex++; }
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.opacity = "1.0";
+    buttons[slideIndex - 1].className = "karusel-navigator white-on-orange";
+    /*if (newIndex == slides.length) { buttonIndex = 1; }
+    else { buttonIndex = newIndex + 1; }*/
+    buttonIndex = newIndex;
+
+}
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides-01");
+    let buttons = document.getElementsByClassName("karusel-navigator");
+    //var texts = document.getElementsByClassName("mySlides-02");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.opacity = "0.0";
+        buttons[i].className = "karusel-navigator white-on-navy";
+        //texts[i].style.opacity = "0.0";
+    }
+    if (buttonIndex) { slideIndex = buttonIndex; buttonIndex = 0; }
+    else { slideIndex++; }
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.opacity = "1.0";
+    buttons[slideIndex - 1].className = "karusel-navigator white-on-orange";
+    //texts[slideIndex - 1].style.opacity = "1.0";
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+showSlides();
