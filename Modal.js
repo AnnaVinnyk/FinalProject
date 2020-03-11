3ea7a99a8f41184ced1c97984e694af8d3941e37
AnnaVinnyk <59229096+AnnaVinnyk@users.noreply.github.com>
2020-03-11 12:31:28 +0200

    Add files via upload

--- Modal.js
+++ Modal.js
@@ -1,12 +1,14 @@
 let modal = document.getElementById('myModal');
 let btn = document.getElementById("myBtn");
-let span = document.getElementsByClassName("close")[0];
+let closeModal = document.getElementsByClassName("close-modal")[0];
 //открытие модального окна
 btn.onclick = function() {
+    document.getElementById('MP-Call').className = 'popuptext';
+    document.getElementById('MP-Online').className = 'popuptext';
     modal.style.display = "block";
 }
 //закрытие модального окна кнопкой Х
-span.onclick = function() {
+closeModal.onclick = function() {
     modal.style.display = "none";
 }
 //закрытие модального окна при нажатии в любом другом месте
